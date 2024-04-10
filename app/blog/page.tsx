e8ffa7c005b1e0aae822e555bfdd0a8d2046eb6b
Lucas Doell <lucas@lux.dev>
2024-04-09 22:42:08 -0400

    Update page title and description in blog posts page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,13 +2,16 @@ import { BlogPosts } from "app/components/posts";
 
 export const metadata = {
   title: "Blog",
-  description: "Read my blog.",
+  description:
+    "Hear about new product updates and learn how we build our products.",
 };
 
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+        Blog Posts
+      </h1>
       <BlogPosts />
     </section>
   );
